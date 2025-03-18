c5ef0b72a64b3fefddb499ee6d2719879b4abbb6
Huang He <162031374+huanghe1223@users.noreply.github.com>
2025-03-18 15:44:52 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"monica-proxy/internal/apiserver"
 	"monica-proxy/internal/config"
@@ -24,8 +25,11 @@ func main() {
 	e.Use(middleware.Recover())
 	// 注册路由
 	apiserver.RegisterRoutes(e)
-	// 启动服务
-	if err := e.Start("0.0.0.0:8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
+	
+	// 使用配置的端口启动服务
+	addr := fmt.Sprintf("0.0.0.0:%d", cfg.Port)
+	log.Printf("Starting server on %s", addr)
+	if err := e.Start(addr); err != nil && !errors.Is(err, http.ErrServerClosed) {
 		log.Fatalf("start server error: %v", err)
 	}
 }
